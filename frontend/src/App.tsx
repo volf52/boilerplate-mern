43c2bf5d1618d5c4edcd8086ba6d3d2709bf17e8
Arslan <rslnkrmt2552@gmail.com>
2019-06-12 16:38:01 +0300

    Sidebar almost done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,47 +42,52 @@ class App extends Component<any, { collapsed: boolean }> {
                                 collapsed={this.state.collapsed}>
                                 <Menu
                                     mode='inline'
-                                    defaultSelectedKeys={['1']}
-                                    defaultOpenKeys={['sub1']}
-                                    style={{ height: '100%', borderRight: 0 }}>
+                                    inlineCollapsed={this.state.collapsed}>
+                                    <Menu.Item key='1'>
+                                        <Icon type='pie-chart' />
+                                        <span>Option 1</span>
+                                    </Menu.Item>
+                                    <Menu.Item key='2'>
+                                        <Icon type='desktop' />
+                                        <span>Option 2</span>
+                                    </Menu.Item>
+                                    <Menu.Item key='3'>
+                                        <Icon type='inbox' />
+                                        <span>Option 3</span>
+                                    </Menu.Item>
                                     <SubMenu
                                         key='sub1'
                                         title={
                                             <span>
-                                                <Icon type='user' />
-                                                subnav 1
+                                                <Icon type='mail' />
+                                                <span>Navigation One</span>
                                             </span>
                                         }>
-                                        <Menu.Item key='1'>option1</Menu.Item>
-                                        <Menu.Item key='2'>option2</Menu.Item>
-                                        <Menu.Item key='3'>option3</Menu.Item>
-                                        <Menu.Item key='4'>option4</Menu.Item>
+                                        <Menu.Item key='5'>Option 5</Menu.Item>
+                                        <Menu.Item key='6'>Option 6</Menu.Item>
+                                        <Menu.Item key='7'>Option 7</Menu.Item>
+                                        <Menu.Item key='8'>Option 8</Menu.Item>
                                     </SubMenu>
                                     <SubMenu
                                         key='sub2'
                                         title={
                                             <span>
-                                                <Icon type='laptop' />
-                                                subnav 2
+                                                <Icon type='appstore' />
+                                                <span>Navigation Two</span>
                                             </span>
                                         }>
-                                        <Menu.Item key='5'>option5</Menu.Item>
-                                        <Menu.Item key='6'>option6</Menu.Item>
-                                        <Menu.Item key='7'>option7</Menu.Item>
-                                        <Menu.Item key='8'>option8</Menu.Item>
-                                    </SubMenu>
-                                    <SubMenu
-                                        key='sub3'
-                                        title={
-                                            <span>
-                                                <Icon type='notification' />
-                                                subnav 3
-                                            </span>
-                                        }>
-                                        <Menu.Item key='9'>option9</Menu.Item>
-                                        <Menu.Item key='10'>option10</Menu.Item>
-                                        <Menu.Item key='11'>option11</Menu.Item>
-                                        <Menu.Item key='12'>option12</Menu.Item>
+                                        <Menu.Item key='9'>Option 9</Menu.Item>
+                                        <Menu.Item key='10'>
+                                            Option 10
+                                        </Menu.Item>
+                                        <SubMenu key='sub3' title='Submenu'>
+                                            <Menu.Item key='11'>
+                                                Option 11
+                                            </Menu.Item>
+                                            <Menu.Item key='12'>
+                                                Option 12
+                                            </Menu.Item>
+                                        </SubMenu>
                                     </SubMenu>
                                 </Menu>
                             </Sider>
